0751e7b59059da5cace5398f88890823070c8128
HwangTaewon <xodnjs2549@naver.com>
2017-01-19 17:14:21 +0900

    edit main window from solid to transparent

--- PKNU_DormitoryMenuPlanner/src/kr/ac/pknu/dormitory/MainClass.java
+++ PKNU_DormitoryMenuPlanner/src/kr/ac/pknu/dormitory/MainClass.java
@@ -1,18 +1,21 @@
 package kr.ac.pknu.dormitory;
 
+import javafx.application.Application;
+
 public class MainClass {
 	public static void main(String[] args) {
-		MenuParsingClass menuParsingClass = new MenuParsingClass();
-		
-		for(int i = 0; i < 4; i++)
-		{
-			System.out.println();
-			for(int j = 0; j < 8; j++)
-			{
-				System.out.println(menuParsingClass.menuPlanner[i][j]);
-			}
-		}
+//		MenuParsingClass menuParsingClass = new MenuParsingClass();
+//		
+//		for(int i = 0; i < 4; i++)
+//		{
+//			System.out.println();
+//			for(int j = 0; j < 8; j++)
+//			{
+//				System.out.println(menuParsingClass.menuPlanner[i][j]);
+//			}
+//		}
 
+		Application.launch(ViewClass.class);
 		
 	}
 }
--- PKNU_DormitoryMenuPlanner/src/kr/ac/pknu/dormitory/ViewClass.java
+++ PKNU_DormitoryMenuPlanner/src/kr/ac/pknu/dormitory/ViewClass.java
@@ -5,111 +5,132 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class ViewClass extends Application{
+public class ViewClass extends Application {
 
 	int rowIndex = 0, columnIndex = 0;
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+	// public static void main(String[] args) {
+	// launch(args);
+	// }
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {		
+	public void start(Stage primaryStage) throws Exception {
 		MenuParsingClass menuParsingClass = new MenuParsingClass();
-		
-		primaryStage.setTitle("ºÎ°æ´ëÇÐ±³ »ýÈ°°ü ½Ä´ÜÇ¥");
+
+		VBox verticalBox = new VBox();
 		StackPane root = new StackPane();
 		GridPane grid = new GridPane();
-		
+
 		Label[][] labelArray = new Label[4][8];
 		String content = null;
-		
-		
-		
-		//Class<?> labelClass = Class.forName("javafx.scene.control.Label"); //reflection
 
-		for(int i = 0; i < 4; i++)
-		{
-			for(int j = 0; j < 8; j++)
-			{
+		// Class<?> labelClass = Class.forName("javafx.scene.control.Label");
+		// //reflection
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 8; j++) {
 				content = menuParsingClass.menuPlanner[i][j];
-				//labelArray[i][j] = (Label) labelClass.newInstance(); //reflection
+				// labelArray[i][j] = (Label) labelClass.newInstance();
+				// //reflection
 				labelArray[i][j] = new Label();
 				labelArray[i][j].setText(content);
-				GridPane.setConstraints(labelArray[i][j], j, i); //ÀÌ °úÁ¤ÀÌ ÀÖ¾î¾ß ¼¿¿¡ Á¢±ÙÀÌ °¡´ÉÇÏ´Ù?
-				if(i == 0) {
-					GridPane.setHalignment(labelArray[i][j], HPos.CENTER );
+				GridPane.setConstraints(labelArray[i][j], j, i); // ÀÌ °úÁ¤ÀÌ ÀÖ¾î¾ß ¼¿¿¡
+																	// Á¢±ÙÀÌ °¡´ÉÇÏ´Ù?
+				if (i == 0) {
+					GridPane.setHalignment(labelArray[i][j], HPos.CENTER);
 					dateDisplay(i, j, labelArray);
 				}
-				if(j == 0) GridPane.setHalignment(labelArray[i][j], HPos.CENTER );
-				
+				if (j == 0)
+					GridPane.setHalignment(labelArray[i][j], HPos.CENTER);
+
 				grid.getChildren().add(labelArray[i][j]);
 			}
 		}
-		//for¾È¿¡¼­ À½..¾ÆÁ÷ µî·Ï?ÀÌ ¾È µÈ ¼¿À» ÀÌ¿ëÇÏ·Á°í ÇÏ¸é null exception ¿¡·¯°¡ ¶á´Ù.
-		//for¹®À» ¹þ¾î³ª¸é ¸ðµç ¼¿ÀÇ µî·ÏÀÌ ¿Ï·áµÈ »óÅÂÀÌ¹Ç·Î ±×¸®µåÀÇ ¸ðµç ¼¿¿¡ ´ëÇÑ Á¢±ÙÀÌ ÀÚÀ¯·Ó´Ù.
+		// for¾È¿¡¼­ À½..¾ÆÁ÷ µî·Ï?ÀÌ ¾È µÈ ¼¿À» ÀÌ¿ëÇÏ·Á°í ÇÏ¸é null exception ¿¡·¯°¡ ¶á´Ù.
+		// for¹®À» ¹þ¾î³ª¸é ¸ðµç ¼¿ÀÇ µî·ÏÀÌ ¿Ï·áµÈ »óÅÂÀÌ¹Ç·Î ±×¸®µåÀÇ ¸ðµç ¼¿¿¡ ´ëÇÑ Á¢±ÙÀÌ ÀÚÀ¯·Ó´Ù.
 		timeDisplay(labelArray);
-		root.getChildren().add(grid);
+		verticalBox.getChildren().add(grid);
+		root.getChildren().add(verticalBox);
+		// root.setAlignment(grid, Pos.CENTER); //¸Å°³º¯¼ö·Î node°¡ µé¾î°¡´Â setAlignment´Â
+		// static¸Þ¼Òµå´Ù. root¸¦ ½áÁÖ¸é °æ°í¶ä ¿Ö?
+		// StackPane.setAlignment(grid, Pos.CENTER_RIGHT);
+		grid.setAlignment(Pos.CENTER); // StackPaneÀ§¿¡ ¿Ã¶ó°¡ÀÖ´Â grid¸¦ °¡¿îµ¥·Î Á¤·ÄÇÑ´Ù.
 		grid.setHgap(10);
 		grid.setVgap(10);
+		// VBoxÀÇ ¸¶Áø°ªÀ» ÁØ´Ù. Insets°´Ã¼¿¡ ´ëÇØ ¾Ë¾Æº¸ÀÚ.
+		// VBox.setMargin(root, new Insets(10, 10, 10, 10));
+		VBox.setMargin(grid, new Insets(30, 10, 15, 10));
+
+		// primaryStage.setScene(new Scene(root, 900, 400));
+		Scene rootScene = new Scene(root, 900, 400);
+		rootScene.setFill(Color.TRANSPARENT);
+		// grid.setStyle("-fx-border-color: burlywood");
+		root.setStyle("-fx-background-color: rgba(252, 228, 236, 0.7);");
+		// primaryStage.setTitle("ºÎ°æ´ëÇÐ±³ »ýÈ°°ü ½Ä´ÜÇ¥");
+		// primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.setScene(rootScene);
+		primaryStage.show();
 		
-		primaryStage.setScene(new Scene(root, 900, 400));
-        primaryStage.show();
+		//ÇÁ·Î±×·¥ÀÇ ¹è°æÀ» Åõ¸íÇÏ°Ô ¸¸µå·Á¸é Stage, Scene, PaneÀ» Åõ¸íÇÏ°Ô ¸¸µé¾îÁà¾ß ÇÑ´Ù.
+		//ÀÌ ¶§ Stage¿Í SceneÀº setStyle()À» Áö¿øÇÏÁö ¾Ê°í Stage´Â initStyle(), SceneÀº setFill()À» ÅëÇÏ¿© Åõ¸íÇÏ°Ô ¸¸µé ¼ö ÀÖ´Ù.
 	}
-	
-	
-	//If today is equal to day of week, that day label border appear red color.
-	public void dateDisplay(int i, int j, Label[][] labelArray)
-	{
+
+	// If today is equal to day of week, that day label border appear red color.
+	public void dateDisplay(int i, int j, Label[][] labelArray) {
 		GetNow getDate = new GetNow();
 		String date = getDate.date();
 		String labelDay = labelArray[i][j].getText();
 		Pattern p = Pattern.compile("[^°¡-ÆR^\\(^\\)]");
 		Matcher m = p.matcher(labelDay);
 		String compareString = "";
-		while(m.find())
-		{
+		while (m.find()) {
 			compareString += m.group();
 		}
-		if(compareString.equals(date))
-		{
+		if (compareString.equals(date)) {
 			System.out.println("i : " + i + "j : " + j);
 			labelArray[i][j].setStyle("-fx-border-color: red;");
 			rowIndex = i;
 			columnIndex = j;
 		}
 	}
-	
+
 	/**
-	 *		if I find date matched today, I will call timeDisplay method in dateDisplay.
+	 * if I find date matched today, I will call timeDisplay method in
+	 * dateDisplay.
 	 */
 	@SuppressWarnings("deprecation")
-	public void timeDisplay(Label[][] labelArray)
-	{
+	public void timeDisplay(Label[][] labelArray) {
 		GetNow getTime = new GetNow();
 		Date nowTime = getTime.time();
-		Date morning = new Date(); //am09:00
-		Date dinner = new Date(); //pm13:40
+		Date morning = new Date(); // am09:00
+		Date dinner = new Date(); // pm13:40
 		morning.setHours(9);
 		morning.setMinutes(0);
 		dinner.setHours(13);
 		dinner.setMinutes(40);
-		
-		if(nowTime.before(morning)) {
+
+		if (nowTime.before(morning)) {
 			System.out.println("now is morning");
 			labelArray[1][columnIndex].setStyle("-fx-border-color: red;");
-		}else if(nowTime.after(dinner)) {
+		} else if (nowTime.after(dinner)) {
 			System.out.println("now is dinner");
-//			System.out.println(labelArray[i][j].toString());
+			// System.out.println(labelArray[i][j].toString());
 			labelArray[3][columnIndex].setStyle("-fx-border-color: red;");
-		}else {
+		} else {
 			System.out.println("now is lunch");
 			labelArray[2][columnIndex].setStyle("-fx-border-color: red;");
 		}
